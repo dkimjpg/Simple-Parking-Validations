122daea696954eb74e75e75e66864f93cb7d6251
David <dkk5396@psu.edu>
2025-01-23 18:16:09 -0500

    Commiting and pushing again because the previous one doesn't seem to push.

--- write.js
+++ write.js
@@ -1,24 +1,31 @@
 //const reader = require('xlsx');
-import XLSX from 'xlsx';
-const workbook = XLSX.readFile('parkingDataTest1.xlsx');
+//import XLSX from 'xlsx';
+const XLSX = require("xlsx");
+const workbook = XLSX.readFile('parkingDataTest2.xlsx');
 const sheetName = 'Sheet1';
 const worksheet = workbook.Sheets[sheetName];
 
 //get last row in sheet
-const lastRow = XLSX.utils.decode_range(worksheet['!ref']).e.r;
+//const lastRow = XLSX.utils.decode_range(worksheet['!ref']).e.r;
 
 // Data to append
 const newData = [
-    ['New Value 1', 'New Value 2'],
-    ['Another Value 1', 'Another Value 2']
+    ["1/23/2025"],
+    ["dkdk"],
+    ["Dav K"]
 ];
 
 // Append data to the sheet
+XLSX.utils.sheet_add_aoa(worksheet, [
+    newData
+], {origin: -1});
+
+/*
 newData.forEach(row => {
     worksheet[`A${lastRow + 1}`] = { v: row[0] };
     worksheet[`B${lastRow + 1}`] = { v: row[1] };
     //lastRow++;
-});
+});*/
 
 // Write the updated workbook to a file
-XLSX.writeFile(workbook, 'parkingDataTest1.xlsx');
+XLSX.writeFile(workbook, 'parkingDataTest2.xlsx');
